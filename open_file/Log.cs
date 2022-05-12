9363e3cf1d8eafde1d496efaf5cef833ff8d3282
ldq <470751984@qq.com>
2022-05-12 23:31:31 +0800

    更换log文件存放位置

--- open_file/Log.cs
+++ open_file/Log.cs
@@ -18,14 +18,14 @@ namespace open_file
             //截取字符串，截取从0到最后⼀个\\出现的位置的字符串  E:\hzrvs\ConsoleApplication1\ConsoleApplication1\bin
             path = path.Substring(0, path.LastIndexOf("\\"));
             //拼串，拼出⾃⼰想要存放的位置的路径   E:\hzrvs\ConsoleApplication1\ConsoleApplication1\bin\logs\11-18\
-            path = path + "\\logs\\" + DateTime.Now.Date.ToString("MM-dd");
+            path = path + "\\XmlData\\";
             //检查这个路径存在吗，不存在就创建
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
             //创建路径后再定义log的文件名
-            logfile = path + "\\" + "log.txt";
+            logfile = path+DateTime.Now.Date.ToString("MM-dd")+"_log.txt";
             writer = new StreamWriter(logfile, true, System.Text.Encoding.UTF8);
             //在⽂件⾥写入日期，TAG和Log信息
             writer.WriteLine(DateTime.Now.ToLocalTime().ToString() + "    " + TAG + "    " + logMessage);
